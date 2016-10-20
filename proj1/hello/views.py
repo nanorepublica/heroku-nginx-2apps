41bc832990c18d76600f535b48e64085f05567e6
Andrew Miller <andrew.miller@founders4schools.org.uk>
2016-10-20 16:50:31 +0100

    add some logging

--- proj1/hello/views.py
+++ proj1/hello/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import logging
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
 def index(request, *args, **kwargs):
+    logger.info('We are the in hello app')
     return HttpResponse('HELLO')
--- proj2/world/views.py
+++ proj2/world/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-
+import logging
 # Create your views here.
+
+logger = logging.getLogger(__name__)
 
 
 def index(request, *args, **kwargs):
+    logger.info('We are the in world app')
     return HttpResponse('WORLD')
