aa748bb987f8d60fe3cae9c563b2846b56648715
Andrew Miller <andrew.miller@founders4schools.org.uk>
2016-10-20 15:47:53 +0100

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/start-nginx python run.py
---
+++ proj1/run.py
@@ -0,0 +1,8 @@
+# run.py
+from waitress import serve
+from proj1 import wsgi
+
+# touch app-initialized
+open('/tmp/app-initialized', 'w').close()
+# start waitress
+serve(wsgi.application, unix_socket='/tmp/nginx.socket')
